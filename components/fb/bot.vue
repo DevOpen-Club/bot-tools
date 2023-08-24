944982293c2310d06628febe35425f77749bbc20
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-08-25 01:25:33 +0800

    feat: 页头显示机器人信息

---
+++ components/fb/bot.vue
@@ -0,0 +1,23 @@
+<!-- 机器人简略信息。 -->
+
+<script lang="ts" setup>
+import { User } from 'fanbook-api-node-sdk';
+
+export interface Props {
+  /** 要展示的机器人资料。 @default DEFAULT_INFO */
+  info: User;
+}
+
+const props = defineProps<Props>();
+
+const avatar = computed(() => {
+  return Reflect.get(props.info, 'avatar') as string | undefined;
+});
+</script>
+
+<template>
+  <ASpace class='h-[30px] overflow-visible' direction='horizontal'>
+    <AAvatar class='w-[30px] h-[30px]' :image-url='avatar' />
+    {{ info.first_name }}
+  </ASpace>
+</template>
