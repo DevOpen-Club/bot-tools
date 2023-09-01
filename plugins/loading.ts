0fbb281c536b8ca8c96b24fed7153201863f53f8
KaiKai <kaikai_miniworld@outlook.com>
2023-09-01 23:59:54 +0800

    fix: 修复返回也显示进度条的问题 (#14)

--- plugins/loading.ts
+++ plugins/loading.ts
@@ -1,11 +1,23 @@
 import nprogress from 'nprogress';
 
-export default defineNuxtPlugin((app) => {
+let first = true;
+/** 开始读进度条。 */
+function start() {
+  if (first) first = false; // 首屏显示的路由不显示进度条
+  else nprogress.start();
+}
+
+export default defineNuxtPlugin(() => {
   nprogress.configure({ showSpinner: false, trickleSpeed: 100, speed: 1000 });
-  app.hook('page:start', () => {
-    nprogress.start();
-  });
-  app.hook('page:finish', () => {
-    nprogress.done();
-  });
+  const router = useRouter();
+  const { push, replace } = useRouter();
+  router.push = (...args) => { // hook to push
+    start();
+    return push(...args);
+  }
+  router.replace = (...args) => { // hook to replace
+    start();
+    return replace(...args);
+  }
+  router.afterEach(() => nprogress.done()); // hook to finish
 });
