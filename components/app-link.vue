5a1ac09e77c823c97824d366375bbe01cb9eb75d
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-09-02 21:00:04 +0800

    fix: 修复默认链接图标非蓝色的问题

--- components/app-link.vue
+++ components/app-link.vue
@@ -23,13 +23,15 @@ const external = computed(() => {
   <ALink v-if='external' v-bind='$attrs' :href='to.toString()' :hoverable='false' target='_blank'>
     <span style='color: initial;'>
       <slot v-if='$slots.icon' name='icon' />
-      <IconLink v-else class='mr-0.5' />
     </span>
+    <IconLink v-if='!$slots.icon' class='mr-0.5' />
     <slot />
   </ALink>
   <RouterLink v-else v-slot='{ navigate }' :to='to' custom>
     <ALink v-bind='$attrs' :hoverable='false' @click='navigate'>
-      <slot name='icon' />
+      <span style='color: initial;'>
+        <slot name='icon' />
+      </span>
       <slot />
     </ALink>
   </RouterLink>
