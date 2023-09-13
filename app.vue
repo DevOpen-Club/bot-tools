4cb6ea265de49072a9ad218c3b0c441809416379
kaikaibenkai <kaikai_miniworld@outlook.com>
2023-09-13 23:11:30 +0800

    feat: 添加顶部内测版本说明

--- app.vue
+++ app.vue
@@ -3,9 +3,15 @@ import 'nprogress/nprogress.css';
 // 手动导入的组件，需要手动导入样式
 import '@arco-design/web-vue/es/message/style/css';
 import '@arco-design/web-vue/es/modal/style/css';
+
+const version = useAppConfig().version;
 </script>
 
 <template>
+  <AAlert v-if='version !== "DEV"' type='warning' closable banner>
+    <template #icon><IconExperiment /></template>
+    版本{{ version.startsWith('PREVIEW') ? '预览' : '内测' }}中，效果请以最终发布为准。
+  </AAlert>
   <PageHeader />
   <ALayoutContent><NuxtPage /></ALayoutContent>
   <PageFooter />
